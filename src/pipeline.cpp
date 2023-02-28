edaeb5937b55d327acdd523cfeee2eb0ab535270
Tyler LeBlond <tylerleblond@gmail.com>
2023-02-28 11:50:01 -0500

    Finished out all two-tile operations.

--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -330,6 +330,44 @@ namespace TISCC
 
                 }
 
+                else if (s == "splitx") {
+
+                    // Measure qsites on the strip in the X basis
+                    double time_tmp = 0;
+                    for (unsigned int site : strip) {
+                        time_tmp = TI_model.add_H(site, time_tmp, 0, grid, hw_master);
+                        time_tmp = TI_model.add_meas(site, time, 1, grid, hw_master);
+                    }
+
+                    // Perform 'idle' operation on each separate qubit
+                    lq1.idle(cycles, grid, hw_master, time);
+                    time = lq2.idle(cycles, grid, hw_master, time);
+
+                }
+
+                else if (s == "bellprepx") {
+
+                    // Prepare x and do an idle on the merged patch
+                    lq.transversal_op("prepx", grid, hw_master, time);
+                    time = lq.idle(cycles, grid, hw_master, time);
+
+                    // Measure qsites on the strip in the X basis
+                    double time_tmp = 0;
+                    for (unsigned int site : strip) {
+                        time_tmp = TI_model.add_H(site, time_tmp, 0, grid, hw_master);
+                        time_tmp = TI_model.add_meas(site, time, 1, grid, hw_master);
+                    }
+
+                    // Perform 'idle' operation on each separate qubit
+                    lq1.idle(cycles, grid, hw_master, time);
+                    time = lq2.idle(cycles, grid, hw_master, time);
+
+                    // Pauli X correction depending on measurement outcome
+
+                    /* TODO: consider whether this op can be done in a single `time step'. */
+
+                }
+
                 // Enforce validity of final instruction list 
                 grid.enforce_hw_master_validity(hw_master);
 
@@ -427,7 +465,7 @@ namespace TISCC
                     // Perform idle on remaining patch
                     time = lq1.idle(cycles, grid, hw_master, time);
 
-                    // Pauli (Z) correction depending on measurement outcome (Z^m) (assumed to be tracked in software for now)
+                    // Pauli (X) correction depending on measurement outcome (X^m) (assumed to be tracked in software for now)
 
                 }
 
@@ -443,6 +481,40 @@ namespace TISCC
 
                     // Measure out the whole merged patch
                     time = lq.transversal_op("measz", grid, hw_master, time);
+
+                }
+
+                else if (s == "splitz") {
+
+                    // Measure qsites on the strip in the Z basis
+                    for (unsigned int site : strip) {
+                        TI_model.add_meas(site, time, 0, grid, hw_master);
+                    }
+
+                    // Perform 'idle' operation on each separate qubit
+                    lq1.idle(cycles, grid, hw_master, time);
+                    time = lq2.idle(cycles, grid, hw_master, time);
+
+                }
+
+                else if (s == "bellprepz") {
+
+                    // Prepare x and idle on the merged patch
+                    lq.transversal_op("prepz", grid, hw_master, time);
+                    time = lq.idle(cycles, grid, hw_master, time);
+
+                    // Measure qsites on the strip in the Z basis
+                    for (unsigned int site : strip) {
+                        TI_model.add_meas(site, time, 0, grid, hw_master);
+                    }
+
+                    // Perform 'idle' operation on each separate qubit
+                    lq1.idle(cycles, grid, hw_master, time);
+                    time = lq2.idle(cycles, grid, hw_master, time);
+
+                    // Pauli Z correction depending on measurement outcome
+
+                    /* TODO: consider whether this op can be done in a single `time step'. */
 
                 }
 
